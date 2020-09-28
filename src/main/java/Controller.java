34b93fd2b9c54def4f1d8348a4829dacf983858d
jbruce <jessbruce37@gmail.com>
2020-09-28 00:16:18 -0400

    Changes made to Product Line Tab: Gridpane added which includes labels with texfields and choicebox. Button added. Label and table view added. Changes made to Produce Tab: Labels added with Listview and Combobox, and a button. Changes to Production Log tab: TextArea added

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,14 +1,25 @@
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class Controller {
 
     @FXML
     private Label lblOutput;
+    @FXML
+    private Button addProduct;
 
     public void sayHello() {
         lblOutput.setText("Hello FXML!");
 
     }
-
+    //Creating an event handler to button click event to print out "Add product"
+    public void addProduct(javafx.event.ActionEvent actionEvent) {
+        System.out.println("Add Product");
+    }
+    //Creating an event handler to button click event to print out "Record Production"
+    public void recordProduction(ActionEvent actionEvent) {
+        System.out.println("Record Production");
+    }
 }
